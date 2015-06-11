cf136c023da24aafe9d2b0b4c85d82e8c23fb97e
Jose Luis Manrique Cabana <kenshin2273@gmail.com>
2015-06-11 15:01:26 -0500

    Implementacion del arbol binario

---
+++ arbol_binario.js
@@ -0,0 +1,99 @@
+function Arbol_Binario(){
+  this.valor = null;
+  this.arbol_izquierdo = null;
+  this.arbol_derecho = null;
+}
+
+Arbol_Binario.prototype.tamanho = function(){
+  var tamanho = 0;
+  if (this.valor != null){
+    if (this.arbol_izquierdo == null && this. arbol_derecho == null){
+      tamanho = 1;
+    } else {
+      tamanho = 1;
+      if (this.arbol_izquierdo != null){
+        tamanho = tamanho + this.arbol_izquierdo.tamanho();
+      }
+      if (this.arbol_derecho != null){
+        tamanho = tamanho + this.arbol_derecho.tamanho();
+      }
+    } 
+  }
+  return tamanho;
+}
+
+Arbol_Binario.prototype.agregar = function(valor){
+  if (this.valor == null){
+    this.valor = valor;
+  } else {
+    if (this.valor > valor){
+      if (this.arbol_izquierdo == null){
+        this.arbol_izquierdo = new Arbol_Binario().agregar(valor);
+      } else {
+        this.arbol_izquierdo.agregar(valor);
+      }
+    } else {
+      if (this.arbol_derecho == null){
+        this.arbol_derecho = new Arbol_Binario().agregar(valor);
+      } else {
+        this.arbol_derecho.agregar(valor);
+      }
+    }
+  }
+  return this;
+}
+
+Arbol_Binario.prototype.listar = function() {
+  var listado = [];
+  if (this.valor != null){
+    if (this.arbol_izquierdo == null && this.arbol_derecho == null){
+      listado.push(this.valor);
+    } else {
+      if (this.arbol_izquierdo != null){
+        var lista_izquierda = this.arbol_izquierdo.listar();
+        lista_izquierda.forEach(function(elemento){
+          listado.push(elemento);
+        });
+      }
+
+      listado.push(this.valor);
+
+      if (this.arbol_derecho != null){
+        var lista_derecha = this.arbol_derecho.listar();
+        lista_derecha.forEach(function(elemento){
+          listado.push(elemento);
+        });
+      }
+    }
+  }
+  return listado;
+};
+
+Arbol_Binario.prototype.encontrar = function(valor){
+  var retorno = null;
+  if (this.valor != null){
+    if (this.valor == valor){
+      return this;
+    } else {
+      if (this.valor > valor){
+        if (this.arbol_izquierdo != null){
+          retorno = this.arbol_izquierdo.encontrar(valor);
+        }
+      } else {
+        if (this.arbol_derecho != null){
+          retorno = this.arbol_derecho.encontrar(valor);
+        }
+      }
+    }
+  }
+  return retorno;
+}
+
+Arbol_Binario.prototype.limpiar = function(){
+  this.valor = null;
+  this.arbol_izquierdo = null;
+  this.arbol_derecho = null;
+  return this;
+}
+
+module.exports = Arbol_Binario;
---
+++ arbol_binario.spec.js
@@ -0,0 +1,86 @@
+var Arbol_Binario = require('./arbol_binario');
+
+describe('Arboles Binarios', function() {
+  
+  var arbol;
+
+  beforeEach(function() {
+    arbol = new Arbol_Binario(); 
+  });
+
+  it('para un arbol vacio el tamanho del arbol es 0', function() {
+     
+     expect(arbol.tamanho()).toBe(0);
+  });
+
+  it('para un arbol con un elemento el tamanho del arbol es 1', function() {
+     
+     arbol.agregar(40);
+     expect(arbol.tamanho()).toBe(1);
+  });
+
+  it('para un arbol con tres elementos el tamanho del arbol es 3', function() {
+     
+     arbol.agregar(40).agregar(10).agregar(60);
+     expect(arbol.tamanho()).toBe(3);
+  });
+
+  it('para un arbol con cuatro elementos el tamanho del arbol es 4', function() {
+     
+     arbol.agregar(40).agregar(10).agregar(60).agregar(7);
+     expect(arbol.tamanho()).toBe(4);
+  });
+
+  it('para un arbol vacio el listar del arbol es un arreglo vacio', function() {
+     
+     expect(arbol.listar()).toEqual([]);
+  });
+
+  it('para un arbol con un elemento el listado del arbol es el elemento', function() {
+     
+     arbol.agregar(40);
+     expect(arbol.listar()).toEqual([40]);
+  });
+
+  it('para un arbol con tres elementos el listado del arbol son los 3 elementos agregados y ordenados', function() {
+     
+     arbol.agregar(40).agregar(10).agregar(60);
+     expect(arbol.listar()).toEqual([10, 40, 60]);
+  });
+
+  it('para un arbol con cuatro elementos el listado del arbol son los 4 elementos agregados y ordenados', function() {
+     
+     arbol.agregar(40).agregar(10).agregar(60).agregar(7);
+     expect(arbol.listar()).toEqual([7, 10, 40, 60]);
+  });
+
+  it('para un arbol con tres elementos el limpiar del arbol retorna un arbol vacio', function() {
+     
+     arbol.agregar(40).agregar(10).agregar(60).limpiar();
+     expect(arbol.tamanho()).toBe(0);
+  });
+
+  it('para un arbol vacio el encontrar un elemento debe retornar null', function() {
+     
+     expect(arbol.encontrar(10)).toBeNull();
+  });
+
+  it('para un arbol con un elemento el encontrar el valor del elemento debe devolver el arbol', function() {
+     
+     arbol.agregar(40);
+     expect(arbol.encontrar(40).listar()).toEqual([40]);
+  });
+
+  it('para un arbol con tres elementos el encontrar el valor de la derecha debe retornar el arbol de la derecha', function() {
+     
+     arbol.agregar(40).agregar(10).agregar(60);
+     expect(arbol.encontrar(60).listar()).toEqual([60]);
+  });
+
+  it('para un arbol con cuatro elementos el encontrar del arbol son los 4 elementos agregados y ordenados', function() {
+     
+     arbol.agregar(40).agregar(10).agregar(60).agregar(7);
+     expect(arbol.encontrar(8)).toBeNull();
+  });
+
+});
